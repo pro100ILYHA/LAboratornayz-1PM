df9e1edd12700ea07535394cb7c61e2a635de8c6
Subbotin <232909073091-23>
2026-04-09 10:26:16 +0300

    Subbotin, Добавлен MusicTrack в Feature

---
+++ MusicTrack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class MusicTrack
+    {
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public MusicTrack(string artist, string title, string genre, int year)
+        {
+            Artist = artist;
+            Title = title;
+            Genre = genre;
+            Year = year;
+        }
+        public override string ToString()
+        {
+            return $"{Artist} - {Title} ({Year}) [{Genre}]";
+        }
+    }
+}
